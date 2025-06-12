06056f33f3da16d4bbc3dcc6049bc8126b28720a
DM <dmaiquez@tecnologicoismac.edu.ec>
2025-06-12 12:26:51 -0500

    mas cambios en autor

--- libreria-spring/src/main/java/com/distribuida/model/Autor.java
+++ libreria-spring/src/main/java/com/distribuida/model/Autor.java
@@ -5,5 +5,19 @@ public class Autor {
     private int idAutor;
     private String nombre;
     private String apellido;
+    private String pais;
+    private String direccion;
+    private String telefono;
 
+    public Autor() {
+    }
+
+    public Autor(int idAutor, String nombre, String apellido, String pais, String direccion, String telefono) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pais = pais;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
 }
